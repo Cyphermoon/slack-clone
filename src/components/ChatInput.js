21e11b4e7a985722b0001af75c54473c274b3f51
Cypher Moon <temperaturecaleb@gmail.com>
2022-08-08 23:25:40 +0100

    the application has added a messages subcollection

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import { collection, doc, getDoc, serverTimestamp, setDoc, Timestamp } from 'firebase/firestore';
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+import { db } from '../firebase';
 
 const ChatInput = ({ channelName }) => {
+    const [input, setInput] = useState("");
+    const roomId = useSelector((state) => state.room.roomId);
 
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        if (!roomId) return false
+
+        const messagesCollectionRef = collection(db, "rooms", roomId, "messages")
+
+        const docSnap = await setDoc(doc(messagesCollectionRef), {
+            content: input,
+            serverTimeStamp: serverTimestamp(),
+            user: "Cypher moon",
+            userImg: "#"
+        })
+
+        console.log(docSnap);
+
+        setInput("")
+    }
 
     return (
-        <StyledChatInput >
-            <input type="text" placeholder={`message ${channelName}`}  />
+        <StyledChatInput onSubmit={(e) => sendMessage(e)} >
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type="text"
+                placeholder={`message ${channelName || "room"}`} />
+
             <button type='submit' hidden>send</button>
         </StyledChatInput>
     )
