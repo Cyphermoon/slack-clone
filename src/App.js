4fce3c36b321153c352149d155652041d08ec54c
Cypher Moon <temperaturecaleb@gmail.com>
2022-11-19 20:08:35 +0100

    coded defensively against mobile devices

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import GamePage from './pages/GamePage';
 import { default as HomePage } from './pages/Home';
 
 function App() {
+  const mql = window.matchMedia('(max-width: 750px)');
+  if (mql.matches) return <h1>This page is yet to be supported on mobile  or tablet</h1>
   return (
     <div className='app' style={{ height: "100vh" }}>
       <Routes>
